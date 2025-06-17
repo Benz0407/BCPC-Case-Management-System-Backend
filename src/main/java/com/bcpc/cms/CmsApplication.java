c813960b02f03e023135e7432adc814d4d22dbe3
Benz0407 <benedick.clerigo@unc.edu.ph>
2025-06-17 09:03:49 +0800

    Implement JWT authentication and database configuration with environment variables

--- src/main/java/com/bcpc/cms/CmsApplication.java
+++ src/main/java/com/bcpc/cms/CmsApplication.java
@@ -1,12 +1,24 @@
 package com.bcpc.cms;
 
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class CmsApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		Properties props = System.getProperties();
+		props.setProperty("DB_URL", dotenv.get("DB_URL"));
+		props.setProperty("DB_USER", dotenv.get("DB_USER"));
+		props.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		props.setProperty("jwt.secret", dotenv.get("JWT_KEY"));
+
 		SpringApplication.run(CmsApplication.class, args);
 	}
 
---
+++ src/main/java/com/bcpc/cms/config/SecurityConfig.java
@@ -0,0 +1,70 @@
+package com.bcpc.cms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.bcpc.cms.service.impl.AuthenticationImpl;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private AuthenticationImpl authService;
+    private JWTAuthFilter jwtAuthFilter;
+
+    public SecurityConfig(AuthenticationImpl authService, JWTAuthFilter jwtAuthFilter) {
+        this.authService = authService;
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(
+                        request -> request
+                                .requestMatchers("/auth/**", "/public/**")
+                                .permitAll()
+                                .requestMatchers("/admin/**")
+                                .hasAnyAuthority("ADMIN")
+                                .requestMatchers("/user/**").hasAnyAuthority("STAFF", "LGU")
+                                .requestMatchers("/adminuser/**").hasAnyAuthority("ADMIN", "STAFF", "LGU")
+                                .anyRequest().authenticated())
+                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider()).addFilterBefore(
+                        jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(authService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
